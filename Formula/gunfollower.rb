e706ee93574974d8e8c0555cf3aa1d49ab189356
Hemanth Krishna <hkpdev008@gmail.com>
2021-12-04 21:13:22 +0530

    fix: sha256 of the release

--- Formula/gunfollower.rb
+++ Formula/gunfollower.rb
@@ -5,7 +5,7 @@ class Gunfollower < Formula
   desc "A Rust CLI to check who unfollows you on GitHub"
   homepage "https://github.com/DarthBenro008/gunfollower"
   url "https://github.com/DarthBenro008/gunfollower/releases/latest/download/gunfollower-macos.tar.gz"
-  sha256 "1a493dec24a8ad1ce782b6da3cfdf6e9c76755da9f8a0047f71d6a2e17ec6755"
+  sha256 "c3a6ee48fdff099792eb0cd081f05de48f669d35cb05747ac7a8ef1657ebb6d1"
   version "0.1.0"
 
   def install
