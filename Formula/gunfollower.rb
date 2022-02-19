d2380da4eaf3da3b9410eeada4e742248c399f7c
Hemanth Krishna <hkpdev008@gmail.com>
2022-02-19 20:54:00 +0530

    bump: v1.0.0

--- Formula/gunfollower.rb
+++ Formula/gunfollower.rb
@@ -2,8 +2,8 @@ class Gunfollower < Formula
   desc "A Rust CLI to check who unfollows you on GitHub"
   homepage "https://github.com/DarthBenro008/gunfollower"
   url "https://github.com/DarthBenro008/gunfollower/releases/latest/download/gunfollower-macos.tar.gz"
-  sha256 "b1e5435ab4ce2ebb6efd05c06a7514e58fdf8fe645ce765673867c3497de08cd"
-  version "0.1.4"
+  sha256 "6dce433687d073427fd676f5c5cdaeac716899bd9b88e3d857eb526997472a48"
+  version "1.0.0"
 
   def install
     bin.install "gunfollower"
